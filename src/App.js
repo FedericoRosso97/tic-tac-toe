7d0ca8821b2ed267f830a55e25e2e5f96eefe6f0
FedericoRosso97 <federicorosso97@gmail.com>
2024-02-19 15:34:23 +0100

    time travel/ reset button

--- src/App.js
+++ src/App.js
@@ -1,43 +1,41 @@
 import { useState } from 'react';
 
 
-function Square({ value, onSquareClick}){
-  return <button className="square" onClick={onSquareClick}>{ value }</button>;
-}
+function Square({ value, onSquareClick }) {
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
+};
 
-
-export default function Board() {
-  const [xIsNext, setXIsNext]=useState(true);
-  const [squares, setSquares]= useState(Array(9).fill(null));
-
-  function handleClick(i){
+function Board({squares, xIsNext, onPlay,ResetGame}) {
+  function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return;
     }
-    
-    const nextSquares= squares.slice();
-    
-    if(xIsNext){
-      nextSquares[i]="X";
-    }else{
-      nextSquares[i]="O";
+
+    const nextSquares = squares.slice(); 
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
     }
-    
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
-  }
+
+    onPlay(nextSquares);
+  };
 
   const winner = calculateWinner(squares);
+  const isDraw = (element) => element === null;
   let status;
-  if(winner){
-    status= "Winner: " + winner;
-  }else{
-    status = 'Next player:' + (xIsNext ? "X" : "O") ;
-  }
-
+  
+  if (winner) {
+    status = "Winner: " + winner;
+  } else if (!(squares.some(isDraw))) {
+    status = "Draw"
+  } else {
+    status = 'Next player:' + (xIsNext ? "X" : "O");
+  };
 
   return (
-  <>
+    <>
       <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
@@ -54,12 +52,67 @@ export default function Board() {
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
-
+      <div>
+        <button className='resetButton' onClick={ResetGame}>New Game</button>
+      </div>
     </>
   );
 }
 
-function calculateWinner(squares){
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+  let xIsNext = currentMove % 2 === 0; 
+
+  function handlePlay (nextSquares){
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function ResetGame() {
+     setCurrentMove(0);
+     setHistory([Array(9).fill(null)]);
+     console.log('reset');
+  };
+
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) =>{
+    let description;
+    if (move > 0){
+      description = 'Vai alla mossa  ' + move;
+    } else {
+      description = 'Vai a inizio partita'
+    }
+    if(move === currentMove){
+      return <li key={move} > {description} </li>
+    }
+    return (
+    <li key={move}>
+      <button onClick={() => jumpTo(move)}>
+       {description}
+      </button>
+    </li>
+   )
+  })
+ 
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={currentSquares} xIsNext={xIsNext} onPlay={handlePlay} ResetGame={ResetGame}/>
+      </div>
+      <div className="game-info">
+        <p>Storia della partita</p>
+        <ol>{moves}</ol>
+      </div>
+    </div>)
+}
+
+function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -71,10 +124,9 @@ function calculateWinner(squares){
     [2, 4, 6]
   ];
 
-  for(let i=0; i<lines.length;i++){
-    const [a,b,c]= lines[i];
-    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-      console.log('ha vinto'+ squares[a])
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a]
     }
   }
